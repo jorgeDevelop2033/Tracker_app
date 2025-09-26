a47dbdd9d5e7d385ca68c43ca5329987b63e2f7b
jorgeDevelop2023 <Jorge.soto.gutierrez.2023@gmail.com>
2025-09-25 21:26:13 -0300

    kjkkk
    kkjk

--- src/Tracker.WebSocketClient/Program.cs
+++ src/Tracker.WebSocketClient/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 
 var connection = new HubConnectionBuilder()
-    .WithUrl("http://192.168.10.32:5137/trackerHub")
+    .WithUrl("http://45.7.228.18:5137/trackerHub")
     .Build();
 
 await connection.StartAsync();
